815441f3785d72269948ef7393a71eb4bbd241ae
Che-Kang Wu <erickitten@gmail.com>
2014-10-28 01:04:23 +0800

    delete function fio_getfd & put OPENFAIL in fio.c
    fio_getfd() provide bound checking to opened file handle
    ,but is unused because fio_is_open_int() has already provided that.
    also ,OPENFAIL is used to replace -1 literals.

--- src/fio.c
+++ src/fio.c
@@ -64,7 +64,7 @@ static ssize_t stdout_write(void * opaque, const void * buf, size_t count) {
     const char * data = (const char *) buf;
     
     for (i = 0; i < count; i++)
-        send_byte(data[i]);
+	send_byte(data[i]);
     
     return count;
 }
@@ -80,37 +80,32 @@ __attribute__((constructor)) void fio_init() {
     fio_sem = xSemaphoreCreateMutex();
 }
 
-//unused?
-struct fddef_t * fio_getfd(int fd) {
-    if ((fd < 0) || (fd >= MAX_FDS))
-        return NULL;
-    return fio_fds + fd;
-}
-
 //check if given file handle is open
 static int fio_is_open_int(int fd) {
 	if ((fd < 0) || (fd >= MAX_FDS)){//bound check
 		return 0;
 	}
     int r = !((fio_fds[fd].fdread == NULL) &&
-              (fio_fds[fd].fdwrite == NULL) &&
-              (fio_fds[fd].fdseek == NULL) &&
-              (fio_fds[fd].fdclose == NULL) &&
-              (fio_fds[fd].opaque == NULL));
+	      (fio_fds[fd].fdwrite == NULL) &&
+	      (fio_fds[fd].fdseek == NULL) &&
+	      (fio_fds[fd].fdclose == NULL) &&
+	      (fio_fds[fd].opaque == NULL));
     return r;
 }
 
 //search for empty space in fio_fds to open to
 static int fio_findfd() {
-    int i;
+	int i;
     
-    for (i = 0; i < MAX_FDS; i++) {
-        if (!fio_is_open_int(i)){
-            return i;
-	}
-    }
-    
-    return -1;
+	for (i = 0; i < MAX_FDS; i++) {
+		if (!fio_is_open_int(i)){
+			return i;
+		}
+	}	
+	
+	//since this function is called by fio_open
+	//this make sense in this context
+	return OPENFAIL;
 }
 
 int fio_is_open(int fd) {
@@ -130,11 +125,11 @@ int fio_open(fdread_t fdread, fdwrite_t fdwrite, fdseek_t fdseek, fdclose_t fdcl
     fd = fio_findfd();
     
     if (fd >= 0) {
-        fio_fds[fd].fdread = fdread;
-        fio_fds[fd].fdwrite = fdwrite;
-        fio_fds[fd].fdseek = fdseek;
-        fio_fds[fd].fdclose = fdclose;
-        fio_fds[fd].opaque = opaque;
+	fio_fds[fd].fdread = fdread;
+	fio_fds[fd].fdwrite = fdwrite;
+	fio_fds[fd].fdseek = fdseek;
+	fio_fds[fd].fdclose = fdclose;
+	fio_fds[fd].opaque = opaque;
     }
     xSemaphoreGive(fio_sem);
     
@@ -168,13 +163,13 @@ ssize_t fio_write(int fd, const void * buf, size_t count) {
     ssize_t r = 0;
 //    DBGOUT("fio_write(%i, %p, %i)\r\n", fd, buf, count);
     if (fio_is_open_int(fd)) {
-        if (fio_fds[fd].fdwrite) {
-            r = fio_fds[fd].fdwrite(fio_fds[fd].opaque, buf, count);
-        } else {
-            r = -3;
-        }
+	if (fio_fds[fd].fdwrite) {
+	    r = fio_fds[fd].fdwrite(fio_fds[fd].opaque, buf, count);
+	} else {
+	    r = -3;
+	}
     } else {
-        r = -2;
+	r = -2;
     }
     return r;
 }
@@ -183,13 +178,13 @@ off_t fio_seek(int fd, off_t offset, int whence) {
     off_t r = 0;
 //    DBGOUT("fio_seek(%i, %i, %i)\r\n", fd, offset, whence);
     if (fio_is_open_int(fd)) {
-        if (fio_fds[fd].fdseek) {
-            r = fio_fds[fd].fdseek(fio_fds[fd].opaque, offset, whence);
-        } else {
-            r = -3;
-        }
+	if (fio_fds[fd].fdseek) {
+	    r = fio_fds[fd].fdseek(fio_fds[fd].opaque, offset, whence);
+	} else {
+	    r = -3;
+	}
     } else {
-        r = -2;
+	r = -2;
     }
     return r;
 }
@@ -198,20 +193,20 @@ int fio_close(int fd) {
     int r = 0;
 //    DBGOUT("fio_close(%i)\r\n", fd);
     if (fio_is_open_int(fd)) {
-        if (fio_fds[fd].fdclose)
-            r = fio_fds[fd].fdclose(fio_fds[fd].opaque);
-        xSemaphoreTake(fio_sem, portMAX_DELAY);
-        memset(fio_fds + fd, 0, sizeof(struct fddef_t));
-        xSemaphoreGive(fio_sem);
+	if (fio_fds[fd].fdclose)
+	    r = fio_fds[fd].fdclose(fio_fds[fd].opaque);
+	xSemaphoreTake(fio_sem, portMAX_DELAY);
+	memset(fio_fds + fd, 0, sizeof(struct fddef_t));
+	xSemaphoreGive(fio_sem);
     } else {
-        r = -2;
+	r = -2;
     }
     return r;
 }
 
 void fio_set_opaque(int fd, void * opaque) {
     if (fio_is_open_int(fd))
-        fio_fds[fd].opaque = opaque;
+	fio_fds[fd].opaque = opaque;
 }
 
 
@@ -225,26 +220,31 @@ devfs (stdio) filesystem
 //open (alternate) handle of stdio
 //since fio_init have already init them (fio_fds[0~2])
 static int devfs_open(void * opaque, const char * path, int flags, int mode) {
-    uint32_t h = hash_djb2((const uint8_t *) path, -1);
+	uint32_t h = hash_djb2((const uint8_t *) path, -1);
+
 //    DBGOUT("devfs_open(%p, \"%s\", %i, %i)\r\n", opaque, path, flags, mode);
-    switch (h) {
-    case stdin_hash:
-        if (flags & (O_WRONLY | O_RDWR))
-            return -1;
-        return fio_open(stdin_read, NULL, NULL, NULL, NULL);
-        break;
-    case stdout_hash:
-        if (flags & O_RDONLY)
-            return -1;
-        return fio_open(NULL, stdout_write, NULL, NULL, NULL);
-        break;
-    case stderr_hash:
-        if (flags & O_RDONLY)
-            return -1;
-        return fio_open(NULL, stdout_write, NULL, NULL, NULL);
-        break;
-    }
-    return -1;
+
+	switch (h) {
+	case stdin_hash:
+		if (flags & (O_WRONLY | O_RDWR)){
+			return OPENFAIL;
+		}
+		return fio_open(stdin_read, NULL, NULL, NULL, NULL);
+		break;
+    	case stdout_hash:
+		if (flags & O_RDONLY){
+			return OPENFAIL;
+		}
+		return fio_open(NULL, stdout_write, NULL, NULL, NULL);
+		break;
+	case stderr_hash:
+		if (flags & O_RDONLY){
+			return OPENFAIL;
+		}
+		return fio_open(NULL, stdout_write, NULL, NULL, NULL);
+		break;
+	}
+	return OPENFAIL;
 }
 
 void register_devfs() {
